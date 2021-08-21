89527cbd66af880085c2a5a59bcf68b15e6ea81c
Alvaro <alvar01omer@gmail.com>
2021-08-20 21:51:23 -0400

    errores en contenedores

--- jobserver/sse-fiuni-jobs/build.sh
+++ jobserver/sse-fiuni-jobs/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd /var/www/html/jobs/
 composer install
 chmod 777 -R storage
-mv .env.example .env
+cp .env.example .env
 php artisan key:generate
--- webserver/sse-fiuni/build.sh
+++ webserver/sse-fiuni/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd /var/www/html/webserver/
 composer install
 chmod 777 -R storage
-mv .env.example .env
+cp .env.example .env
 php artisan key:generate
