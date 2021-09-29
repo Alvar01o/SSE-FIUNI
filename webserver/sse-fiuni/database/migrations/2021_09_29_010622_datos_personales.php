e2a1aa71ac8acfbdc6cdddf5680379165c8d9d67
Alvaro <alvar01omer@gmail.com>
2021-09-28 21:51:24 -0400

    cambios en contenedores y algunas migraciones

---
+++ webserver/sse-fiuni/database/migrations/2021_09_29_010622_datos_personales.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DatosPersonales extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datos_personales', function (Blueprint $table) {
+            $table->id();
+            $table->string('telefono');
+            $table->string('nombre');
+            $table->integer('ci')->unique();
+            $table->string('apellido');
+            $table->integer('id_carrera');
+            $table->integer('id_usuario');
+            $table->dateTime('egreso');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datos_personales');
+    }
+}
---
+++ webserver/sse-fiuni/database/migrations/2021_09_29_010646_laboral.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Laboral extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laboral', function (Blueprint $table) {
+            $table->id();
+            $table->string('empresa');
+            $table->string('cargo');
+            $table->dateTime('inicio');
+            $table->dateTime('fin')->nullable();
+            $table->integer('id_usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laboral');
+    }
+}
